52dc0cf927a9dbc69cc38f4d02103449fc15874b
Your Name <logan.lentz1@gmail.com>
2023-12-14 18:05:43 -0600

    higher resolution full image

--- GIBS-Geo.py
+++ GIBS-Geo.py
@@ -18,37 +18,22 @@ import numpy as np
 from owslib.wms import WebMapService
 from IPython.display import Image, display
 
-# Construct Web Mercator projection URL.
-proj3857 = 'https://gibs.earthdata.nasa.gov/wms/epsg3857/best/wms.cgi?\
-version=1.3.0&service=WMS&\
-request=GetMap&format=image/png&STYLE=default&bbox=-8000000,-8000000,8000000,8000000&\
-CRS=EPSG:3857&HEIGHT=600&WIDTH=600&TIME=2000-12-01&layers=Landsat_WELD_CorrectedReflectance_Bands157_Global_Annual'
+# Connect to GIBS WMS Service
+wms = WebMapService('https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?', version='1.1.1')
 
-# Request image.
-img=io.imread(proj3857)
+# Configure request for MODIS_Terra_CorrectedReflectance_TrueColor
+img = wms.getmap(layers=['MODIS_Terra_CorrectedReflectance_TrueColor'],  # Layers
+                 srs='epsg:4326',  # Map projection
+                 bbox=(-180,-90,180,90),  # Bounds
+                 size=(2800, 1200),  # Image size
+                 time='2023-12-14',  # Time of data
+                 format='image/png',  # Image format
+                 transparent=True)  # Nodata transparency
 
-# Display image on map.
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1, projection = ccrs.Mercator.GOOGLE)
-extent = (-8000000, 8000000, -8000000, 8000000)
-plt.imshow(img, transform = ccrs.Mercator.GOOGLE, extent = extent, origin = 'upper')
+# Save output PNG to a file
+out = open('images/MODIS_Terra_CorrectedReflectance_TrueColor.png', 'wb')
+out.write(img.read())
+out.close()
 
-# Draw grid.
-gl = ax.gridlines(ccrs.PlateCarree(), linewidth = 1, color = 'blue', alpha = 0.3,  draw_labels = True)
-gl.top_labels = False
-gl.right_labels = False
-gl.xlines = True
-gl.ylines = True
-gl.xlocator = mticker.FixedLocator([0, 30, -30, 0])
-gl.ylocator = mticker.FixedLocator([-30, 0, 30])
-gl.xformatter = LONGITUDE_FORMATTER
-gl.yformatter = LATITUDE_FORMATTER
-gl.xlabel_style = {'color': 'blue'}
-gl.ylabel_style = {'color': 'blue'}
-
-plt.title('WMS Landsat Reflectance In Web Mercator Projection',
-          fontname = "Times New Roman", fontsize = 20, color = 'green')
-
-plt.show()
-
-print('')
+# View image
+Image('images/MODIS_Terra_CorrectedReflectance_TrueColor.png')
--- GIBS-Request.py
+++ GIBS-Request.py
@@ -29,7 +29,7 @@ img = wms.getmap(layers=['MODIS_Terra_CorrectedReflectance_TrueColor'],  # Layer
                  srs='epsg:4326',  # Map projection
                  bbox=texas_bbox,  # Bounds for Texas
                  size=(250, 250),  # Image size
-                 time='2023-12-13',  # Time of data
+                 time='2023-12-14',  # Time of data
                  format='image/png',  # Image format
                  transparent=True)  # Nodata transparency
 
